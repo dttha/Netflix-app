ef7d2fe88eb8af16e96087ae913c99bae5b6f7cb
đttha <dothithuha140@gmail.com>
2022-01-27 15:01:02 +0700

    fix collapse

--- src/components/Collapse/index.js
+++ src/components/Collapse/index.js
@@ -5,8 +5,58 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
 function Collapse() {
     const boxId = useRef(null)
+    const boxId1 = useRef(null)
+    const boxId2 = useRef(null)
+    const boxId3 = useRef(null)
+    const boxId4 = useRef(null)
+    const boxId5 = useRef(null)
     const onToggleClick = (e) => {
         boxId.current.classList.toggle(styles["question-content-show-add"]);
+        boxId1.current.classList.remove(styles["question-content-show-add"]);
+        boxId2.current.classList.remove(styles["question-content-show-add"]);
+        boxId3.current.classList.remove(styles["question-content-show-add"]);
+        boxId4.current.classList.remove(styles["question-content-show-add"]);
+        boxId5.current.classList.remove(styles["question-content-show-add"]);
+    };
+    const onToggleClick1 = (e) => {
+        boxId1.current.classList.toggle(styles["question-content-show-add"]);
+        boxId.current.classList.remove(styles["question-content-show-add"]);
+        boxId2.current.classList.remove(styles["question-content-show-add"]);
+        boxId3.current.classList.remove(styles["question-content-show-add"]);
+        boxId4.current.classList.remove(styles["question-content-show-add"]);
+        boxId5.current.classList.remove(styles["question-content-show-add"]);
+    };
+    const onToggleClick2 = (e) => {
+        boxId2.current.classList.toggle(styles["question-content-show-add"]);
+        boxId1.current.classList.remove(styles["question-content-show-add"]);
+        boxId.current.classList.remove(styles["question-content-show-add"]);
+        boxId3.current.classList.remove(styles["question-content-show-add"]);
+        boxId4.current.classList.remove(styles["question-content-show-add"]);
+        boxId5.current.classList.remove(styles["question-content-show-add"]);
+    };
+    const onToggleClick3 = (e) => {
+        boxId3.current.classList.toggle(styles["question-content-show-add"]);
+        boxId1.current.classList.remove(styles["question-content-show-add"]);
+        boxId2.current.classList.remove(styles["question-content-show-add"]);
+        boxId.current.classList.remove(styles["question-content-show-add"]);
+        boxId4.current.classList.remove(styles["question-content-show-add"]);
+        boxId5.current.classList.remove(styles["question-content-show-add"]);
+    };
+    const onToggleClick4 = (e) => {
+        boxId4.current.classList.toggle(styles["question-content-show-add"]);
+        boxId1.current.classList.remove(styles["question-content-show-add"]);
+        boxId2.current.classList.remove(styles["question-content-show-add"]);
+        boxId3.current.classList.remove(styles["question-content-show-add"]);
+        boxId.current.classList.remove(styles["question-content-show-add"]);
+        boxId5.current.classList.remove(styles["question-content-show-add"]);
+    };
+    const onToggleClick5 = (e) => {
+        boxId5.current.classList.toggle(styles["question-content-show-add"]);
+        boxId1.current.classList.remove(styles["question-content-show-add"]);
+        boxId2.current.classList.remove(styles["question-content-show-add"]);
+        boxId3.current.classList.remove(styles["question-content-show-add"]);
+        boxId4.current.classList.remove(styles["question-content-show-add"]);
+        boxId.current.classList.remove(styles["question-content-show-add"]);
     };
     return <div className={styles["question"]}>
         <div className={styles["question-wrap"]}>
@@ -29,22 +79,22 @@ function Collapse() {
                 </div>
 
                 <div className={styles["question-content-dropdown"]}>
-                    <div className={styles["question-main"]} onClick={onToggleClick}>
+                    <div className={styles["question-main"]} onClick={onToggleClick1}>
                         <h6>How much does Netflix cost?</h6>
                         <FontAwesomeIcon icon={faPlus} style={{ fontSize: 10, color: "#fff", width: 20, height: 20 }}></FontAwesomeIcon>
                     </div>
-                    <div className={styles["question-content-show"]} ref={boxId}>
+                    <div className={styles["question-content-show"]} ref={boxId1}>
                         <span className={styles["question-content-desc"]}>Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.
                         </span>
                     </div>
                 </div>
 
                 <div className={styles["question-content-dropdown"]}>
-                    <div className={styles["question-main"]} onClick={onToggleClick}>
+                    <div className={styles["question-main"]} onClick={onToggleClick2}>
                         <h6>Where can I watch?</h6>
                         <FontAwesomeIcon icon={faPlus} style={{ fontSize: 10, color: "#fff", width: 20, height: 20 }}></FontAwesomeIcon>
                     </div>
-                    <div className={styles["question-content-show"]} ref={boxId}>
+                    <div className={styles["question-content-show"]} ref={boxId2}>
                         <span className={styles["question-content-desc"]}>Watch anywhere, anytime. Sign in with your Netflix account to watch
                             instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app,
                             including smart TVs, smartphones, tablets, streaming media players and game consoles.<br /><br />
@@ -54,33 +104,33 @@ function Collapse() {
                 </div>
 
                 <div className={styles["question-content-dropdown"]}>
-                    <div className={styles["question-main"]} onClick={onToggleClick}>
+                    <div className={styles["question-main"]} onClick={onToggleClick3}>
                         <h6>How do I cancel?</h6>
                         <FontAwesomeIcon icon={faPlus} style={{ fontSize: 10, color: "#fff", width: 20, height: 20 }}></FontAwesomeIcon>
                     </div>
-                    <div className={styles["question-content-show"]} ref={boxId}>
+                    <div className={styles["question-content-show"]} ref={boxId3}>
                         <span className={styles["question-content-desc"]}>Netflix is flexible. There are no pesky contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.
                         </span>
                     </div>
                 </div>
 
                 <div className={styles["question-content-dropdown"]}>
-                    <div className={styles["question-main"]} onClick={onToggleClick}>
+                    <div className={styles["question-main"]} onClick={onToggleClick4}>
                         <h6>What can I watch on Netflix?</h6>
                         <FontAwesomeIcon icon={faPlus} style={{ fontSize: 10, color: "#fff", width: 20, height: 20 }}></FontAwesomeIcon>
                     </div>
-                    <div className={styles["question-content-show"]} ref={boxId}>
+                    <div className={styles["question-content-show"]} ref={boxId4}>
                         <span className={styles["question-content-desc"]}>Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want.
                         </span>
                     </div>
                 </div>
 
                 <div className={styles["question-content-dropdown"]}>
-                    <div className={styles["question-main"]} onClick={onToggleClick}>
+                    <div className={styles["question-main"]} onClick={onToggleClick5}>
                         <h6>Is Netflix good for kids?</h6>
                         <FontAwesomeIcon icon={faPlus} style={{ fontSize: 10, color: "#fff", width: 20, height: 20 }}></FontAwesomeIcon>
                     </div>
-                    <div className={styles["question-content-show"]} ref={boxId}>
+                    <div className={styles["question-content-show"]} ref={boxId5}>
                         <span className={styles["question-content-desc"]}>The Netflix Kids experience is included in your membership to
                             give parents control while kids enjoy family-friendly TV shows and movies in their own space.<br /><br />
                             Kids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see.
